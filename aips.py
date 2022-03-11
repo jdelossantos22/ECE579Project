4d0ac311c04328e92a8191af6c39371756fa6d71
jdelossantos22 <jdelossantos22@email.arizona.edu>
2022-03-10 19:16:39 -0700

    adding aips

---
+++ aips.py
@@ -0,0 +1,11 @@
+class Shelf:
+    def __init__(self):
+        return
+    
+class Stand:
+    def __init__(self):
+        return
+
+class Chilled_Stand:
+    def __init__(self):
+        return
---
+++ graph.py
@@ -0,0 +1,7 @@
+class Edge:
+    def __init__(a,b):
+        return
+
+class Node:
+    def __init__(name):
+        id = name
