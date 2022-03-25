7b2d2aa9ae283a198e9823ad85ee68229fda00b8
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2022-03-24 20:34:14 -0700

    wip: hw3

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,20 +1,29 @@
+from asyncio import new_event_loop
 from heapq import heappush, heappop
 import copy
+from mimetypes import init
+from queue import PriorityQueue
 import pandas as pd
 import numpy as np
 MOVES = [[-1,0,1],
          [-1,0,1]]
+LEFT = (0,-1)
+RIGHT = (0,1)
+UP = (0,-1)
+DOWN = (0,1)
+MOVES = [LEFT,RIGHT,UP, DOWN]
 class Graph:
     def __init__(self):
         return
     
 class Node:
-    def __init__(self, parent, state, empty_tile_pos, cost, level):
+    def __init__(self, parent, state, empty_tile_pos, cost,heuristic, level):
         self.parent = parent
         self.state = state
         self.empty_tile_pos = empty_tile_pos
         self.cost = cost
         self.level = level
+        self.a_score = cost+heuristic
         return
     def __lt__(self,nxt):
         return self.cost < nxt.cost
@@ -53,13 +62,36 @@ def get_loc(goal,val):
 def printMatrix(mat):
     print(pd.DataFrame(mat).to_string(index=False, header=False))
     
+def find_empty_tile(mat):
+    location = (0,0)
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            if(mat[i][j] == -1):
+                return (i,j)
+    return location
+            
+    
 def a_star(initial, goal, heuristic):
+    #openList = []
+    #closedList = []
+    root = Node(None,
+                initial,
+                find_empty_tile(initial),
+                0,
+                heuristic(initial, goal),
+                0)
+    openList = PriorityQueue()
     print(heuristic(initial,goal))
+    while not openList:
+        #generate all children from current node
+        #add to queue
+        #sort queueu by a_score
+        #call 
+        continue
     return
 
 def main():
-    openList = []
-    closedList = []
+    
     initialState = [[2,8,6],
                     [1,6,4],
                     [7,-1,5]]
@@ -67,7 +99,8 @@ def main():
                 [8,-1,4],
                 [7,6,5]]
     printMatrix(initialState)
-    a_star(initialState, goalState, manhattan)
+    a_star(initialState, goalState, euclidean)
+    a_star(initialState,goalState, manhattan)
     
     
 if __name__ == "__main__":
