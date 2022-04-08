22acadc2cf3bf3a1604b4d535f627050f931ee3e
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2022-04-07 20:35:17 -0700

    working on gui

--- gui_example.py
+++ gui_example.py
@@ -72,6 +72,23 @@ class ControlFrame(ttk.LabelFrame):
         super().__init__(container)
         self['text'] = 'Options'
 
+        # radio buttons
+        self.selected_value = tk.IntVar()
+
+        ttk.Radiobutton(
+            self,
+            text='F to C',
+            value=0,
+            variable=self.selected_value,
+            command=self.change_frame).grid(column=0, row=0, padx=5, pady=5)
+
+        ttk.Radiobutton(
+            self,
+            text='C to F',
+            value=1,
+            variable=self.selected_value,
+            command=self.change_frame).grid(column=1, row=0, padx=5, pady=5)
+
         self.grid(column=0, row=1, padx=5, pady=5, sticky='ew')
 
         # initialize frames
@@ -80,6 +97,10 @@ class ControlFrame(ttk.LabelFrame):
             container,
             'Please enter the number of customers for simulation: ',
             TemperatureConverter.fahrenheit_to_celsius)
+        self.frames[1] = ConverterFrame(
+            container,
+            'Please enter the number of customers for simulation: ',
+            TemperatureConverter.celsius_to_fahrenheit)
 
         self.change_frame()
 
