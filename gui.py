7291e61c5e94dcf16c09f7637527e0e258cfb9f7
jdelossantos22@email.arizona.edu <jdelossantos22@email.arizona.edu>
2022-04-13 23:24:37 -0700

    working on gui

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import itertools
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
@@ -5,26 +6,56 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
+import aips
+
 kivy.require("2.1.0")
 
 class ConnectPage(GridLayout):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(ConnectPage, self).__init__(**kwargs)
         self.cols = 2
         
         self.add_widget(Label(text="Please enter the number of customers for simulation: "))
-        self.numCustomers = TextInput(multiline=False)
-        self.add_widget(self.numCustomers)
+        self.customers = TextInput(multiline=False, input_filter="int")
+        self.add_widget(self.customers)
         
-        self.proceed = Button(text="Proceed")
-        self.proceed.bind(on_press=self.initiate_sim)
-        self.add_widget(Label())
-        self.add_widget(self.proceed)
+        self.submit = Button(text="Submit", font_size=32)
+        self.submit.bind(on_press=self.initiate_sim)
+        self.empty = Label()
+        self.add_widget(self.empty)
+        self.add_widget(self.submit)
+    
+    def initiate_sim(self, instance):
+        self.numCustomers = int(self.customers.text)
+        self.custNames = []
+        self.remove_widget(self.empty)
+        self.remove_widget(self.submit)
+        #name of customers
+        for i in range(self.numCustomers):
+            self.add_widget(Label(text=f"Please enter name for customer {i}: "))
+            self.custNames.append(TextInput(multiline=False))
+            self.add_widget(self.custNames[i])
+            
+        #new submit button
+        self.submit.bind(on_press=self.ask_distance)
+        self.add_widget(self.submit)
+        
+    def ask_distances(self, instance):
+        #remove buttons for now
+        self.remove_widget(self.empty)
+        self.remove_widget(self.submit)
+        
+        numComb = itertools.combinations(self.customers,2)
+        #ask for distance between customers and 
+        for i in range(self.numCustomers):
+            pass
+    def run_sim(self, instance):
+        pass
 
-class EpicApp(App):
+class AIDApp(App):
     def build(self):
         return ConnectPage()
 
 if __name__ == "__main__":
-    EpicApp().run()
+    AIDApp().run()
     
